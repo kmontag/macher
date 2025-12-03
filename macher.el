824fe4bcab82082bd0ca8f4809adbc2511ca3dad
Kevin Montag <kmontag@cs.stanford.edu>
2025-12-03 04:25:07 +0100

    Add license header to main package file

--- macher.el
+++ macher.el
@@ -1,10 +1,29 @@
 ;;; macher.el --- LLM implementation toolset -*- lexical-binding: t -*-
 
+;; Copyright (C) 2025 Kevin Montag
+
 ;; Author: Kevin Montag
 ;; Version: 0.3.0
 ;; Package-Requires: ((emacs "30.1") (gptel "0.9.8.5"))
 ;; Keywords: convenience, gptel, llm
 ;; URL: https://github.com/kmontag/macher
+
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;; This file is NOT part of GNU Emacs.
 
 ;;; Commentary:
 ;; macher provides a toolset for project-aware LLM file editing based on gptel.
